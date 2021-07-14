f3df1881146331b654356347251145d7ce050115
jrpbjr <jrpbjr@gmail.com>
2021-07-14 14:14:01 -0300

    inicio projeto geocode

---
+++ src/test/java/com/gama/geocode/GeocodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gama.geocode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeocodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
