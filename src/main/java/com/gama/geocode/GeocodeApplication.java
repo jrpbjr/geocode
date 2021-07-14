224234cae2d344ee9afb23859b0714c52afb7db4
jrpbjr <jrpbjr@gmail.com>
2021-07-14 14:17:39 -0300

    inico configurando consulta

--- src/main/java/com/gama/geocode/GeocodeApplication.java
+++ src/main/java/com/gama/geocode/GeocodeApplication.java
@@ -9,5 +9,20 @@ public class GeocodeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GeocodeApplication.class, args);
 	}
+	
+	/*@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}*/
+	
+	/*@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			System.out.println("-----> " + env.getProperty("apiKey"));
+			GeoCoding geoCoding = restTemplate.getForObject(
+					"https://maps.googleapis.com/maps/api/geocode/json?address=SLS+Sapphire&key=AIzaSyCEHvPwo2BLLzjaJ-YPkLm2syxIy", GeoCoding.class);
+			log.info(geoCoding.toString());
+		};
+	}*/
 
 }
